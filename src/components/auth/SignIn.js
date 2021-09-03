7cb5c9ccd6cbe62885229039c53516e85eff5c25
shareef-dweikat <shareef.dweikat@gmail.com>
2021-09-03 12:08:47 +0300

    version number

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -127,7 +127,7 @@ const SignIn = ({ loginUser, isAuthenticated, massage }) => {
               {translate("Don't have an account?")} <a href="/signup">{translate("Sign Up")}</a>
             </div>
               <div style={{textAlign: 'center'}}>
-              1.0.0 V
+              0.0.1 V
              </div>
           </div>
          
